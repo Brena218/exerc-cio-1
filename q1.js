e7dc0a0c9b936fb5173f1154d27bf2e0503382cc
Brena <fbrena218@gmail.com>
2024-09-17 15:15:28 -0300

    feito

---
+++ q1.js
@@ -0,0 +1,63 @@
+// 1)
+
+// a)
+let A = 35;
+let B = 45;
+console.log(B);
+B = 10;
+console.log(A);
+console.log(B);
+
+// b)
+A = 22;
+B = 33;
+C = A+B;
+console.log(C);
+B = 5;
+console.log(B);
+console.log(C);
+C = A+B;
+console.log(A);
+console.log(B);
+console.log(C);
+
+// c)
+A = 60;
+B = 20;
+C = A;
+B = C;
+A = B;
+console.log(A);
+console.log(B);
+console.log(C);
+
+// d)
+A = 10;
+B = A+5;
+A = B+3;
+B = A+5;
+console.log(A);
+A = B+2;
+console.log(A);
+console.log(B);
+
+// e)
+A = 50;
+B = 25;
+C = A+B;
+B = 20;
+A = 10;
+console.log(A);
+
+// f)
+X = 1;
+Y = 2;
+Z = X-Y;
+console.log(Z);
+X = 5;
+Y = X+Z;
+console.log(X);
+console.log(Z-Y);
+
+
+
